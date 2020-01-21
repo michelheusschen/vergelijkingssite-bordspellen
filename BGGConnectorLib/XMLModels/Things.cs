acedcdd523a91bc6ff82bf6dce8ecd80cbf28d7f
Michel Heusschen <1829696heusschen@zuyd.nl>
2020-01-21 07:40:36 +0100

    Voeg Poll toe aan Things model

--- BGGConnectorLib/XMLModels/Things.cs
+++ BGGConnectorLib/XMLModels/Things.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace BGGConnectorLib.XMLModels
 {
     public class Things
     {
+        public class Poll
+        {
+            public class Result
+            {
+                [XmlAttribute("value")]
+                public string Value { get; set; }
+
+                [XmlAttribute("numvotes")]
+                public int NumVotes { get; set; }
+            }
+
+            public class ResultsElement
+            {
+                [XmlAttribute("numplayers")]
+                public string NumPlayers { get; set; }
+
+                [XmlElement("result")]
+                public Result[] Results { get; set; }
+            }
+
+            [XmlAttribute("name")]
+            public string Name { get; set; }
+
+            [XmlAttribute("title")]
+            public string Title { get; set; }
+
+            [XmlAttribute("totalvotes")]
+            public int TotalVotes { get; set; }
+
+            [XmlElement("results")]
+            public ResultsElement[] Results { get; set; }
+        }
     }
 }
