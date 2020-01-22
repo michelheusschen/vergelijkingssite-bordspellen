1d1d59a5dcd6ab7da45bab8e3a7470410f4079e6
Zhyxxy <jordykleijkers@gmail.com>
2020-01-22 09:11:57 +0100

    playingtime,minplaytime,maxplaytime,minage,link

--- BGGConnectorLib/XMLModels/Things.cs
+++ BGGConnectorLib/XMLModels/Things.cs
@@ -36,6 +36,75 @@ namespace BGGConnectorLib.XMLModels
 
             [XmlElement("results")]
             public ResultsElement[] Results { get; set; }
+            [XmlRoot("playingtime")]
+            public class Playingtime
+            {
+                [XmlAttribute(AttributeName = "value")]
+                public string Value { get; set; }
+            }
+
+            [XmlRoot("minplaytime")]
+            public class Minplaytime
+            {
+                [XmlAttribute(AttributeName = "value")]
+                public string Value { get; set; }
+            }
+
+            [XmlRoot("maxplaytime")]
+            public class Maxplaytime
+            {
+                [XmlAttribute(AttributeName = "value")]
+                public string Value { get; set; }
+            }
+
+            [XmlRoot("minage")]
+            public class Minage
+            {
+                [XmlAttribute(AttributeName = "value")]
+                public string Value { get; set; }
+            }
+
+            [XmlRoot("link")]
+            public class Link
+            {
+                [XmlAttribute(AttributeName = "type")]
+                public string Type { get; set; }
+
+                [XmlAttribute(AttributeName = "id")]
+                public string Id { get; set; }
+
+                [XmlAttribute(AttributeName = "value")]
+                public string Value { get; set; }
+            }
+
+
+
+            [XmlRoot("item")]
+            public class Thing
+            {
+                [XmlAttribute("playingtime")]
+                public Playingtime Playingtime { get; set; }
+
+                [XmlAttribute("minplaytime")]
+                public Minplaytime Minplaytime { get; set; }
+
+                [XmlAttribute("maxplaytime")]
+                public Maxplaytime Maxplaytime { get; set; }
+
+                [XmlAttribute("minage")]
+                public Minage Minage { get; set; }
+
+                [XmlAttribute("link")]
+                public Link Link { get; set; }
+
+
+            }
+
+            [XmlElement("item")]
+            public Thing[] Items { get; set; }
+
+            [XmlElement("links")]
+            public Link[] Links { get; set; }
         }
     }
 }
